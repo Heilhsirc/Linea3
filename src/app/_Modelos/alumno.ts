be27905a8c583913e91b5c3aafbeec69be67468d
heilhsirc <moshimio@hotmail.com>
2023-04-26 09:34:37 -0500

    Se crea el proyecto

---
+++ src/app/_Modelos/alumno.ts
@@ -0,0 +1,7 @@
+export class Alumno {
+    id: number | undefined;
+    nif:string | undefined;
+    nombre:string | undefined;;
+    apellido1:string | undefined;;
+    apellido2:string | undefined;;
+}
